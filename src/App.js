9c8ba47a61802f4847b6ca7099ea427cc6d46f95
Mads Ward <whatstheward@gmail.com>
2022-04-28 08:04:33 -0400

    set up app html structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import "./app/sass/styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className='main-header'>Chuck Norris</h1>
+        <h2 className='secondary-header'>Random Facts</h2>
       </header>
+      <div className='main-body'>
+        
+      </div>
     </div>
   );
 }
